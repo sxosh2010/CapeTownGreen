6b1b6d8e24f5da4e8cacdb7e58ee17864200bb66
coder <xolelwa@projectcodex.com>
2016-02-17 17:00:14 +0200

    first 5 function done

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
- 
+ // var body = document.querySelector("body");
+ // body.onkeydown = function(e){
+ // 	//e.keycodedown -will capture the key codes
+ // });
+
+
  var keyCodeName = function(keyCode){
- if(keyCode=== 38){
- 		return "up";
- 	}
- if(keyCode=== 39){
- 		return "right";
- 	}
- if(keyCode===40){
- 		return "down";
- 	}
  if(keyCode=== 37){
  		return "left";
+ 	}
+  else if(keyCode=== 39){
+ 		return "right";
+ 	}
+ else if(keyCode===40){
+ 		return "down";
+ 	}
+ else if(keyCode=== 38){
+ 		return "up";
  	}
  else {
  		return "";
  	}
  };
  
- 	var el = document.getElementById("keyCode");
- 	if (el.addEventListener) {
- 	        el.addEventListener("click", Function, false);
- 	    } else {
- 	        el.attachEvent('onclick', Function);
- 	    }  
+//  	var str = document.getElementById("keyCode");
+//  	if (str.addEventListener) {
+//  	        str.addEventListener("click", myFunction(){
+//  	    } else {
+//  	        str.attachEvent('onclick', myFunction);
+//  	    } 
+ 	    
+// });
 
- 	
+//  .onkeydown = function(e){
+//  	var str = mykeycodes(e.keyCode);
+//  	display Message(str);
+//  }
+//  
