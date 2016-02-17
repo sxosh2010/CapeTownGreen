720b5f8ff5d2367ee6ae8353cc4645899d8f12f8
coder <xolelwa@projectcodex.com>
2016-02-17 11:20:33 +0200

    first function done

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-
-var body = document.querySelector("body");
-
-body.onkeydown = function(e){
-    //e.keyCode - will capture the key codes
-};
+ 
+ var keyCodeName = function(keyCode){
+ if(keyCode=== 38){
+ 		return "up";
+ 	}
+ if(keyCode=== 39){
+ 		return "right";
+ 	}
+ if(keyCode===40){
+ 		return "down";
+ 	}
+ if(keyCode=== 37){
+ 		return "left";
+ 	}
+ else {
+ 		return "";
+ 	}
+ };
